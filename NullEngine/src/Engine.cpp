af7b5be6c6cb6dace6d726e0f9314f2a47547d5c
Samuel Šimůnek <samuel.simunek@live.com>
2022-10-23 20:29:52 +0200

    added size of cube to randvecs so that cubes don't overlap

--- NullEngine/src/Engine.cpp
+++ NullEngine/src/Engine.cpp
@@ -414,7 +414,7 @@ int Engine::Main()
       //lightShader->SetVec3("lightPos", newLightPos);
 
       model = glm::mat4(1.0f);
-      model = glm::translate(model, cubePositions[0] + randvecs[i]);
+      model = glm::translate(model, cubePositions[0] + randvecs[i] + glm::normalize(randvecs[i]) * 2.0f);
       model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f) * 3.0f);
 
       angle = rotTime * (-20.0f);
